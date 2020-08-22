849343fc3ab76521d9d2626effc548f70da1875c
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-08-21 22:09:41 -0400

    feat: the initial card using unsplash data is added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-6xl">
-        Welcome
-      </h1>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+            Photo by Eleazar Maestre
+        </div>
+      </div>
     </div>
   );
 }
