7f1be8e54bf6a56ae37edde9556d41b2996ae40f
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-08-22 19:14:03 -0400

    feat: the tag section is added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,31 @@ function App() {
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
       <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
+        <div className="font-bold text-blue-500 text-xl mb-2">
             Photo by Eleazar Maestre
         </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>300
+          </li>
+          <li>
+            <strong>Likes: </strong>500
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
       </div>
     </div>
   );
