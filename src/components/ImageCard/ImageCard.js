36b57b57f5950873ebc7009aa4b69a58c9dc8a7b
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-09-02 22:13:25 -0400

    feat: the image card component is created

---
+++ src/components/ImageCard/ImageCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function ImageCard(props) {
+    const { image, ...restProps } = props;
+
+    return (
+        <div
+            className="max-w-sm rounded overflow-hidden shadow-lg"
+            {...restProps}
+        >
+            <img src={image.webformatURL} alt="" className="w-full" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-blue-500 text-xl mb-2">
+                    Photo by Eleazar Maestre
+                </div>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>4000
+                    </li>
+                    <li>
+                        <strong>Downloads: </strong>300
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>500
+                    </li>
+                </ul>
+            </div>
+            <div className="px-6 py-4">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                    #tag1
+                </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                    #tag2
+                </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                    #tag3
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default ImageCard;
