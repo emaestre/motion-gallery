8b31fe318fe5531c78e65ca94d4165d2219d07c2
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-10-05 23:01:00 -0400

    fix: the unnecessary useMemos are removed

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -3,11 +3,8 @@ import React, { useMemo } from 'react';
 function ImageCard(props) {
     const { image, ...restProps } = props;
 
-    const imageSource = useMemo(() => image.webformatURL, [image]);
-    const user = useMemo(() => image.user, [image]);
-    const views = useMemo(() => image.views, [image]);
-    const downloads = useMemo(() => image.downloads, [image]);
-    const likes = useMemo(() => image.likes, [image]);
+    const { webformatURL, user, views, downloads, likes } = image;
+
     const tags = useMemo(() => image.tags.split(','), [image]);
 
     return (
@@ -15,7 +12,7 @@ function ImageCard(props) {
             className="max-w-sm rounded overflow-hidden shadow-lg"
             {...restProps}
         >
-            <img src={imageSource} alt="" className="w-full" />
+            <img src={webformatURL} alt="" className="w-full" />
             <div className="px-6 py-4">
                 <div className="font-bold text-blue-500 text-xl mb-2">
                     Photo by {user}
